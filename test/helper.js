02931a0bc5c16ce03311f50ee83c2fb190b60b18
Thomas <thomas.b.huang@gmail.com>
2020-01-08 04:18:41 -0800

    Update helpers

--- test/helper.js
+++ test/helper.js
@@ -32,6 +32,9 @@ require('jsdom-global')()
 // localStorage
 window.localStorage = {}
 
+// override metamask-logo
+window.requestAnimationFrame = () => {}
+
 // crypto.getRandomValues
 if (!window.crypto) {
   window.crypto = {}
